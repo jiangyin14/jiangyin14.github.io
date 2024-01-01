bf9f2859586690421d66d9a0edaecf633431be99
姜胤 <1219439938@qq.com>
2024-01-01 13:07:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm796070704-c55c729cc887deae910a5d4f', //思锐下载站
+ //   'm784490063-7b5da437e7f1e0d67613714d',
+ //   'm784497419-de55aa09902ccb3ab22d548a',
+ //   'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
 
   // 日志天数
@@ -22,15 +22,15 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'https://www.srinternet.top/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/srinternet'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: 'Bilibili',
+      url: 'https://space.bilibili.com/1969160969/'
     },
   ],
 };
