1410dc08aab605eacd0c93d57e2251f01e9980c6
姜胤 <1219439938@qq.com>
2024-01-01 13:20:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm796070704-c55c729cc887deae910a5d4f', //思锐下载站
+    'm796071070-1f39415c2571f85c45962405', //思锐Cloudreve
  //   'm784490063-7b5da437e7f1e0d67613714d',
  //   'm784497419-de55aa09902ccb3ab22d548a',
  //   'm784496436-71a4bf7b1e3bdf7756be131b',
